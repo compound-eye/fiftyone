06a029b6cf8e9b151e48b693af59075e1fa61823
Scott Wu <scott@compoundeye.com>
2023-01-11 09:03:27 -0800

    app: show values from all layers in the tooltip on hover (#3)
    Comments and naming

--- app/packages/core/src/components/Modal/TooltipInfo.tsx
+++ app/packages/core/src/components/Modal/TooltipInfo.tsx
@@ -55,7 +55,7 @@ const ContentItem = ({
         {(() => {
           switch (typeof value) {
             case "number":
-              return Number.isInteger(value) ? value : value.toFixed(3);
+              return Number.isInteger(value) ? value : value.toPrecision(4);
             case "string":
               return value.length ? value : '""';
             case "boolean":
@@ -108,9 +108,8 @@ export const TooltipInfo = React.memo(() => {
     display: detail ? "block" : "none",
     opacity: detail ? 1 : 0,
   });
-  const Component = detail ? OVERLAY_INFO[detail.type] : null;
 
-  return Component
+  return detail
     ? ReactDOM.createPortal(
         <TooltipDiv
           style={{ ...coordsProps, ...showProps, position: "fixed" }}
@@ -121,7 +120,7 @@ export const TooltipInfo = React.memo(() => {
           {detail.label.tags && detail.label.tags.length > 0 && (
             <TagInfo key={"tags"} tags={detail.label?.tags} />
           )}
-          <Component key={"attrs"} detail={detail} />
+          <AllOverlayInfo key={"attrs"} detail={detail} />
         </TooltipDiv>,
         document.body
       )
@@ -269,6 +268,25 @@ const PolylineInfo = ({ detail }) => {
     </AttrBlock>
   );
 };
+
+/** Component that lists values from all overlays of the hovered item */
+function AllOverlayInfo(props: {
+  detail: { color: string; target: any; overlayDetails: any[] };
+}): JSX.Element {
+  const { detail } = props;
+  const { overlayDetails = [] } = detail;
+  return (
+    <AttrBlock style={{ borderColor: detail.color }}>
+      {overlayDetails.map((overlay) => (
+        <ContentItem
+          key={`pixel-value-${overlay.field}`}
+          name={overlay.field}
+          value={overlay.target}
+        />
+      ))}
+    </AttrBlock>
+  );
+}
 
 const OVERLAY_INFO = {
   Classification: ClassificationInfo,
