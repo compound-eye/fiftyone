a570b41f6cd9a8f3f139a769bcdae3d95fc4468a
Scott Wu <scott@compoundeye.com>
2023-03-29 15:16:02 -0700

    env: modify Dockerfile for development (#18)

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+DIR=$(dirname $(realpath $0))
+FIFTYONE_SRC=$DIR
+PORT_OPTS=${PORT_OPTS:--p 5151:5151 -p 5173:5173}
+
+docker run \
+    -it \
+    --mount "type=bind,source=$FIFTYONE_SRC,target=/src" \
+    --mount "type=bind,source=/,target=/host" \
+    -u "$(id -u $USER):$(id -g $USER)" \
+    ${PORT_OPTS} \
+    ce_fiftyone_dev bash
