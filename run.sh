10b472a0814a94eb45d5a8124d671d98f0bdc009
Scott Wu <scott@compoundeye.com>
2023-08-21 11:30:41 -0700

    env: use RUN --mount during build

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ PORT_OPTS=${PORT_OPTS:--p 5151:5151 -p 5173:5173}
 
 docker run \
     -it \
-    --mount "type=bind,source=$FIFTYONE_SRC,target=/src" \
+    --mount "type=bind,source=$FIFTYONE_SRC,target=/src51" \
     --mount "type=bind,source=/,target=/host" \
     -u "$(id -u $USER):$(id -g $USER)" \
     ${PORT_OPTS} \
